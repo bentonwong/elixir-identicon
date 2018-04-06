b16a2638932de6da01ec53ef02f5d9d3f61a5313
bentonwong <bentonwong@gmail.com>
2018-04-06 13:39:48 -0700

    add #filter_odd_squares and stub out #build_pixel_map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,12 +4,31 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} =  image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
